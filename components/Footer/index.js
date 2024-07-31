80477370b1a68c7d8a9298b3963805dfb1c3df87
unknown <bugialfaridzi@gmail.com>
2024-07-31 11:33:15 +0700

    bugialfaridi-portfolio

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -16,8 +16,12 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
             </h1>
-            <Button type="primary">Schedule a call</Button>
             <div className="mt-10">
+              <Button type="primary"
+                onClick={() => window.open("https://wa.me/085974419850")}
+              >
+                Schedule a chat
+              </Button>
               <Socials />
             </div>
           </div>
