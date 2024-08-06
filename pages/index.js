bc809c2c8a0187973f431195ad0fa30d6a70c2d3
unknown <bugialfaridzi@gmail.com>
2024-08-06 11:09:24 +0700

    'bugialfaridi-portfolio'

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import Head from "next/head";
 import Button from "../components/Button";
 import Link from "next/link";
 import Cursor from "../components/Cursor";
+import { Analytics } from "@vercel/analytics/react"
 
 // Local Data
 import data from "../data/portfolio.json";
